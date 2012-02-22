7140846522217791d8b82580df62a7a99d090930
Sloan D. Mergler <smergler@gmail.com>
2012-02-22 14:35:46 -0500

    changing paths for when its used as a library

--- dialog.js
+++ dialog.js
@@ -1,10 +1,18 @@
 function showDialog(title, message) {
+	setPaths();
 	$.facebox('<h2 class="dialog_title"><span>' + title + '</span></h2><div class="dialog_content"><div class="dialog_body">' + message + '</div></div>');
-} 
+
+}
 function showErrorDialog(title, message) {
+	setPaths();
 	$.facebox('<h2 class="dialog_title"><span>' + title + '</span></h2><div class="dialog_content"><div class="dialog_body">' + message + '</div></div>');
-} 
+}
 function showErrorDialogContextual(el, title, message) {
+	setPaths();
 	$.facebox('<h2 class="dialog_title"><span>' + title + '</span></h2><div class="dialog_content"><div class="dialog_body">' + message + '</div></div>');
 }
 
+function setPaths() {
+	$.facebox.settings.loadingImage = 'lib/facebox/facebox/loading.gif';
+	$.facebox.settings.closeImage = 'lib/facebox/facebox/closelabel.gif';
+}
